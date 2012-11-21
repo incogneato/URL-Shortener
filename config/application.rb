be4c9a3a60128e2c2de3f40f011cee272cca02e3
Michael Wiggins <wigginsmichaelc@gmail.com>
2012-11-21 15:09:55 -0800

    fix initializer for heroku and devise

--- config/application.rb
+++ config/application.rb
@@ -59,5 +59,8 @@ module UrlShortener
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    #heroku and devise
+    config.assets.initialize_on_precompile = false
   end
 end
